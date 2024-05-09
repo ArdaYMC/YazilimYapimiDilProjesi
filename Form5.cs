3a77db072fba19bef334dcc3312fac12bd9f66d5
HuseyinnKaan <h_tasgeldi@hotmail.com>
2024-05-09 22:33:41 +0300

    Form 5 update
    Soru sayısını hesaplamadaki bug giderildi

--- Form5.cs
+++ Form5.cs
@@ -1,32 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace deneme2
 {
     public partial class Form5 : Form
     {
+        
         public Form5()
         {
             InitializeComponent();
         }
-
+        int questionCount;
         private void button1_Click(object sender, EventArgs e)
-        {
-            QuizEkrani quiz = new QuizEkrani();
-            quiz.Show();
-            this.Hide();
+        { 
+            // Kullanıcı girdisi alma
+            if (!int.TryParse(textBox1.Text, out int questionCount) || questionCount <= 0)
+            {
+                MessageBox.Show("Geçerli bir sayı giriniz.");
+                return ;
+            }
+
+            // Quiz ekranını aç
+            QuizEkrani quizEkrani = new QuizEkrani(questionCount);
+            quizEkrani.Show();
+
+            this.Close();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Uygulamayı kapat
             Application.Exit();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
