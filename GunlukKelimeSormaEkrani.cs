f905abc27ac0ca4828a93d603a92cd0ab39d8cf8
HuseyinnKaan <h_tasgeldi@hotmail.com>
2024-05-17 04:04:19 +0300

    Kelime ekleme modülü update
    Yeni girilen kelimede kullanıcının o kelimedeki seviyesi otomatik olarak 0 atanıyor ve herhangi bir kullanıcının hangi kelimede hangi seviyede olduğu bilgisi veri tabanına kaydediliyor.

---
+++ GunlukKelimeSormaEkrani.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace deneme2
+{
+    public partial class GunlukKelimeSormaEkrani : Form
+    {
+        
+        public GunlukKelimeSormaEkrani()
+        {
+            InitializeComponent();
+        }
+        int questionCount;
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            // Kullanıcı girdisi alma
+            if (!int.TryParse(textBox1.Text, out int questionCount) || questionCount <= 0)
+            {
+                MessageBox.Show("Geçerli bir sayı giriniz.");
+                return ;
+            }
+
+            // Quiz ekranını aç
+            QuizEkrani quizEkrani = new QuizEkrani(questionCount);
+            quizEkrani.Show();
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Uygulamayı kapat
+            Application.Exit();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- QuizEkrani.Designer.cs
+++ QuizEkrani.Designer.cs
@@ -28,44 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(23, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Soru1 : ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(220, 98);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(290, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = " Kelimesinin Türkçe Karşılığı nedir?";
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(91, 193);
+            this.radioButton1.Location = new System.Drawing.Point(177, 204);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(103, 20);
             this.radioButton1.TabIndex = 0;
@@ -76,7 +55,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(368, 193);
+            this.radioButton2.Location = new System.Drawing.Point(376, 204);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(103, 20);
             this.radioButton2.TabIndex = 1;
@@ -87,7 +66,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(91, 274);
+            this.radioButton3.Location = new System.Drawing.Point(177, 253);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(103, 20);
             this.radioButton3.TabIndex = 2;
@@ -98,7 +77,7 @@
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(368, 273);
+            this.radioButton4.Location = new System.Drawing.Point(376, 253);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(103, 20);
             this.radioButton4.TabIndex = 3;
@@ -106,51 +85,57 @@
             this.radioButton4.Text = "radioButton4";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(650, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(535, 344);
+            this.button1.Location = new System.Drawing.Point(249, 348);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Cevabı Onayla";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Soruyu Onayla";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // label1
             // 
-            this.button2.Location = new System.Drawing.Point(535, 405);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Quizden Çıkış yap";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(134, 98);
+            this.label3.Location = new System.Drawing.Point(148, 119);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 22);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "..........";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(26, 415);
+            this.progressBar1.Location = new System.Drawing.Point(151, 415);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(445, 23);
+            this.progressBar1.Size = new System.Drawing.Size(313, 23);
             this.progressBar1.TabIndex = 9;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.ImageLocation = "";
-            this.pictureBox1.Location = new System.Drawing.Point(571, 61);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(203, 187);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // QuizEkrani
             // 
@@ -158,16 +143,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "QuizEkrani";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QuizEkrani";
@@ -179,16 +163,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
